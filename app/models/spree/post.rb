1a6e92be617f34fcf435d035ad7189560d22ff23
louisscruz <louisstephancruz@me.com>
2014-08-07 12:52:35 -0400

    Await stackoverflow

--- app/models/spree/post.rb
+++ app/models/spree/post.rb
@@ -2,7 +2,6 @@ class Spree::Post < ActiveRecord::Base
   validates_presence_of :title, :description, :event_date
 
   def event_date
-    
   end
 
   scope :published, lambda { where(:published => true) }
