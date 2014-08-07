283fd0ecb78bdaccb12686ba16b9a953226d6163
louisscruz <louisstephancruz@me.com>
2014-08-07 11:00:42 -0400

    Add event to post model

--- app/models/spree/post.rb
+++ app/models/spree/post.rb
@@ -1,6 +1,9 @@
 class Spree::Post < ActiveRecord::Base
   validates_presence_of :title, :description, :event
 
+  def event
+  end
+
   scope :published, lambda { where(:published => true) }
   scope :latest, order("created_at DESC").limit(3)
 end
