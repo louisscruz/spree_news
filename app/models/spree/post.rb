adbea6218f5e5d6249cf9e8f3b2da0a9441c56c3
louisscruz <louisstephancruz@me.com>
2014-08-08 10:12:00 -0400

    Add composed of to model

--- app/models/spree/post.rb
+++ app/models/spree/post.rb
@@ -1,8 +1,10 @@
 class Spree::Post < ActiveRecord::Base
   validates_presence_of :title, :description, :event_date
-
-  def event_date
-  end
+  composed_of :event_date,
+              :class_name => 'Date',
+              :mapping => %w(Date to_s),
+              :constructor => Proc.new{ |item| item },
+              :converter => Proc.new{ |item| item }
 
   scope :published, lambda { where(:published => true) }
   scope :latest, order("created_at DESC").limit(3)
