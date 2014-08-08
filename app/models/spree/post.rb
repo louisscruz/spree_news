20f62ce34cb5c3471e6085ad4f912c03d4be00e1
louisscruz <louisstephancruz@me.com>
2014-08-08 11:25:35 -0400

    remove model composed of

--- app/models/spree/post.rb
+++ app/models/spree/post.rb
@@ -1,10 +1,6 @@
 class Spree::Post < ActiveRecord::Base
   validates_presence_of :title, :description, :event_date
-  composed_of :event_date,
-              :class_name => 'Time',
-              :mapping => %w(Time to_s),
-              :constructor => Proc.new{ |item| item },
-              :converter => Proc.new{ |item| item }
+  attr_accessor :event_date
 
   scope :published, lambda { where(:published => true) }
   scope :latest, order("created_at DESC").limit(3)
