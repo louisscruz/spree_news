2578bc806d17990b3d5efb45a83460972891b06d
louisscruz <louisstephancruz@me.com>
2014-08-12 14:36:11 -0400

    Change db

--- db/migrate/20120312183513_create_posts.rb
+++ db/migrate/20120312183513_create_posts.rb
@@ -3,10 +3,10 @@ class CreatePosts < ActiveRecord::Migration
     create_table :spree_posts do |t|
       t.string :title
       t.text :description
-      t.text :event_month
-      t.text :event_day
-      t.text :event_year
-      t.text :event_time
+      t.string :event_month
+      t.string :event_day
+      t.string :event_year
+      t.string :event_time
       t.boolean :published
 
       t.timestamps
