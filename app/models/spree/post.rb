05082d97804b7cc90d58e1793d3bfbe50c4d4d6f
louisscruz <louisstephancruz@me.com>
2014-08-12 14:17:05 -0400

    Back to attr_accessor

--- app/models/spree/post.rb
+++ app/models/spree/post.rb
@@ -1,17 +1,6 @@
 class Spree::Post < ActiveRecord::Base
   validates_presence_of :title, :description, :event_month, :event_day, :event_year, :event_time
-
-  def :event_month
-  end
-
-  def :event_day
-  end
-
-  def :event_year
-  end
-
-  def :event_time
-  end
+  attr_accessor :event_month, :event_day, :event_year, :event_time
 
   scope :published, lambda { where(:published => true) }
   scope :latest, order("created_at DESC").limit(3)
