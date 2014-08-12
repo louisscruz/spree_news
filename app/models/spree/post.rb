8dfd3eea721a79adde93eb4b2841cb7674e2d438
louisscruz <louisstephancruz@me.com>
2014-08-12 14:04:09 -0400

    Change MVC to event_month, day, year, and time

--- app/models/spree/post.rb
+++ app/models/spree/post.rb
@@ -1,6 +1,6 @@
 class Spree::Post < ActiveRecord::Base
-  validates_presence_of :title, :description, :event_date
-  attr_accessor :event_date
+  validates_presence_of :title, :description, :event_month, :event_day, :event_year, :event_time
+  attr_accessor :event_day, :event_day, :event_year, :event_time
 
   scope :published, lambda { where(:published => true) }
   scope :latest, order("created_at DESC").limit(3)
