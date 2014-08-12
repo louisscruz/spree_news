e67868443fc822427c160ffefae809e1ae297ee5
louisscruz <louisstephancruz@me.com>
2014-08-12 14:13:55 -0400

    Def events

--- app/models/spree/post.rb
+++ app/models/spree/post.rb
@@ -1,6 +1,17 @@
 class Spree::Post < ActiveRecord::Base
   validates_presence_of :title, :description, :event_month, :event_day, :event_year, :event_time
-  attr_accessor :event_day, :event_day, :event_year, :event_time
+
+  def :event_month
+  end
+
+  def :event_day
+  end
+
+  def :event_year
+  end
+
+  def :event_time
+  end
 
   scope :published, lambda { where(:published => true) }
   scope :latest, order("created_at DESC").limit(3)
